b48e5f9370a67754aafbb3a9b98664caf70be178
weipeng <ytwppp@163.com>
2022-01-22 15:22:15 +0800

    后端1

---
+++ new-year-service/src/main/java/wang/yeting/newyear/NewYearServiceApplication.java
@@ -0,0 +1,13 @@
+package wang.yeting.newyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewYearServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewYearServiceApplication.class, args);
+    }
+
+}
---
+++ new-year-service/src/test/java/wang/yeting/newyear/NewYearServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wang.yeting.newyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewYearServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
